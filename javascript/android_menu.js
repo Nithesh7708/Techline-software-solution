990c019a58f37350e31edba0a47d5e66bfb9a943
Nithesh7708 <117974041+Nithesh7708@users.noreply.github.com>
2024-02-25 08:07:06 +0530

    add menu slider

---
+++ javascript/android_menu.js
@@ -0,0 +1,11 @@
+// menu bar open and close section
+function menu_open(){
+   document.getElementById('menu_bar_android').style.display='block';
+   document.getElementById('menu_bar_top').style.display='none';
+   document.getElementById('menu_bar_top_close').style.display='block'
+}
+function menu_close(){
+ document.getElementById('menu_bar_android').style.display='none';
+ document.getElementById('menu_bar_top_close').style.display='none'
+ document.getElementById('menu_bar_top').style.display='block';
+}
