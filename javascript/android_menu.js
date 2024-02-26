13fbd7d152a27b54f06a90ad3c9b1fbb4f1f3bc9
Nithesh7708 <117974041+Nithesh7708@users.noreply.github.com>
2024-02-26 19:17:28 +0530

    i have extra designed menu android

--- javascript/android_menu.js
+++ javascript/android_menu.js
@@ -1,11 +1,15 @@
 // menu bar open and close section
-function menu_open(){
-   document.getElementById('menu_bar_android').style.display='block';
-   document.getElementById('menu_bar_top').style.display='none';
-   document.getElementById('menu_bar_top_close').style.display='block'
-}
-function menu_close(){
- document.getElementById('menu_bar_top_close').style.display='none'
- document.getElementById('menu_bar_android').style.display='none'
- document.getElementById('menu_bar_top').style.display='block';
-}
+function menu_open() {
+   document.getElementById("menu_bar_android").style.display = "block";
+   document.getElementById("menu_bar_top_close").style.display = "block";
+   document.getElementById("menu_bar_top").style.display = "none";
+ }
+ 
+ function menu_close() {
+   document.getElementById("menu_bar_android").style.display = "none";
+   document.getElementById("menu_bar_top_close").style.display = "none";
+   document.getElementById("menu_bar_top").style.display = "block";
+ }
+// function menu_closed_foceus(){
+//    aleart("closed")
+// }
